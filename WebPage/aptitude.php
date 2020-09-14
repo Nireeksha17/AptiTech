8f267ecbda85e2f94a1ee37e718d40be4be288cc
Dpk Bhat <ddbhat99@gmail.com>
2020-09-15 01:54:12 +0530

    aptitude.php formatted well

--- WebPage/aptitude.php
+++ WebPage/aptitude.php
@@ -5,99 +5,75 @@ include 'front.php';
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Aptitude: AptiTech</title>
 	<style type="text/css">
-      table, th, td {
-  border: none;
-  border-collapse: collapse;
-  background-color: white;
-  color: black;
-}
-th, td {
-  padding: 25px;
+	table, th, td {
+		border: none;
+		border-collapse: collapse;
+		background-color: white;
+		color: black;
+	}
+	th, td {
+		padding: 25px;
+		text-align: left;
+		color: white;
+		font-weight: 20px;
+	}
+	* {
+		box-sizing: border-box;
+	}
 
-  text-align: left;
-  color: white;
+	input[type="button"]{
+		font-size: 20px;
+	}
+	.row .a-tag .btn{
+		text-decoration: none;
+		color: white;
+		font-size: 20px;
+	}
 
-  font-weight: 20px;
-}
-* {
-  box-sizing: border-box;
-}
-
-input[type="button"]{
-  font-size: 20px;
-}
-
-    </style>
+	.row .a-tag .btn {
+		padding: 20px;
+		background-color:#005461;
+		color:white;
+		width: 150px;
+		white-space: normal;
+		height:100px;'
+	}
+	</style>
 </head>
 <body>
-
 	<div style="padding-left:20px; padding-right: 20px; padding-top: 5px;">
-      <h1 align="center" style="color: black; text-decoration: underline; text-decoration: none;">APTITUDE</h1>
-      <center>
-      <table><tr>
-      <?php
-$conn = mysqli_connect("localhost", "root", "", "aptitech");
-$query = "SELECT * FROM topic where cat_id = 1";
-$result = mysqli_query($conn, $query);
-$num_of_button = 6;
-while ($row = $result->fetch_assoc()) {
-    echo "<td style='color:white;text-decoration:none;'>
-          <button  style='padding: 20px;background-color:#005461;color:white; width: 150px;white-space: normal; height:100px;'><a href='../Content/" . $row['cat_id'] . "-" . $row['topic_id'] . ".pdf' target='_blank' rel='noopener noreferrer'>" . $row['topic_name'] . "</a></button>
-          </td>
-          ";
-    if(--$num_of_button == 0){
-      echo"
-          </tr><tr>
-      ";
-      $num_of_button = 6;
-    }
-}
-//' name='submit-test'   .$row['topic_name'].
-?>
-
-    </tr><br></table></center>
-  </body>
+		<h1 align="center" >APTITUDE</h1>
+		<center>
+		<table>
+			<tr>
+				<?php
+					$conn = mysqli_connect("localhost", "root", "", "aptitech");
+					$query = "SELECT * FROM topic where cat_id = 1";
+					$result = mysqli_query($conn, $query);
+					$num_of_button = 6;
+					while ($row = $result->fetch_assoc()) {
+						echo "
+							<td class = 'row'>
+									<a href='../Content/" . $row['cat_id'] . "-" . $row['topic_id'] . ".pdf' class = 'a-tag' target='_blank' rel='noopener noreferrer'>
+										<button class = 'btn'>
+											" . $row['topic_name'] . "
+										</button>
+									</a>
+							</td>
+						";
+						if(--$num_of_button == 0){
+							echo"
+								</tr><tr>
+							";
+							$num_of_button = 6;
+						}
+					}
+				?>
+			</tr><br>
+		</table>
+		</center>
+	</div>
+</body>
 </html>
-      <!--<table style="margin-left: 25%;">
-
-
-            <tr>
-              <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal; height:100px;" type="button" value="Ages"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal;height:100px;" type="button" value="Percentage"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal;height:100px;" type="button" value="Simple Interest"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal;height:100px;" type="button" value="Time and Distance"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal;height:100px;" type="button" value="Boats and Streams"></td>
-
-            </tr>
-            <tr>
-              <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal;height:100px;" type="button" value="LCM & HCF"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px; white-space: normal;height:100px;" type="button" value="Probability"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Pipes & Cistern"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal;height:100px;" type="button" value="Bankers Discount"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Permutation & combination"></td>
-
-            </tr>
-            <tr>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Mensuration"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Time & Work"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Ratio & Propotion"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Mixture & Alligation"></td>
-              <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px; white-space: normal;height:100px;" type="button" value="Data Interpretation"></td>
-
-            </tr>
-      		<tr>
-      			<td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Calenders"></td>
-      			<td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height:100px;" type="button" value="Clock"></td>
-      			 <td><input style="padding: 20px;background-color:#005461;color: white; width: 150px;white-space: normal;height: 100px;" type="button" value="Number System"></td>
-      			 <td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height: 100px;" type="button" value="Number Series"></td>
-      			<td><input style="padding: 20px;background-color:#005461;color: white;width: 150px;white-space: normal;height: 100px;" type="button" value="Data Sufficiency"></td>
-
-      		</tr>
-
-
-        </table>;-->
-
-
-
--- WebPage/first.php
+++ WebPage/first.php
@@ -19,12 +19,11 @@ if (!isset($_SESSION) || !isset($_POST['submit'])) {
   $_SESSION["max_time"] = $max_time;
   $_SESSION["start_time"] = date("y-m-d H:i:s");
   $end_time = date('y-m-d H:i:s', strtotime('+'.$_SESSION["max_time"].'minutes', strtotime($_SESSION["start_time"])));
-
   $_SESSION["end_time"] = $end_time;
+
   if(isset($_POST['submit'])){
     $_SESSION['submit'] = $_POST['submit'];
   }
- 
 ?>  
 <script type="text/javascript">
 window.location = 'test.php';
